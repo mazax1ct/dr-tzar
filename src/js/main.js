6dccb68c3d474e3f45868bf9916ef0bf0d607b12
mazax1ct <vova@giveandget.ru>
2021-07-06 17:46:25 +0300

    1

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+$(document).on('click', '.js-params-toggler', function () {
+  if($('body').width() < 767) {
+    $(this).find('svg').toggleClass('rotate');
+    $(this).next('.params__inner').slideToggle();
+  }
+  
+  return false;
+});
+
+$(document).on('click', '.info', function () {
+  if($(this).hasClass('is-open')) {
+    $(this).removeClass('is-open');
+  } else {
+    $(this).addClass('is-open');
+  }
+  return false;
+});
