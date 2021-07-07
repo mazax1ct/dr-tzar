ec1f25567129f6847f69667c378973fa3038ec33
mazax1ct <vova@giveandget.ru>
2021-07-07 18:06:25 +0300

    2

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,45 @@
+//тоглер параметров на мобилах
 $(document).on('click', '.js-params-toggler', function () {
   if($('body').width() < 767) {
     $(this).find('svg').toggleClass('rotate');
     $(this).next('.params__inner').slideToggle();
   }
-  
   return false;
 });
 
+//тултипы
 $(document).on('click', '.info', function () {
   if($(this).hasClass('is-open')) {
     $(this).removeClass('is-open');
   } else {
     $(this).addClass('is-open');
   }
+  return false;
+});
+
+//откртие фильтра
+$(document).on('click', '.js-filter-opener', function () {
+  $('.games__filter').addClass('is-open');
+  return false;
+});
+
+//закрытие фильтра
+$(document).on('click', '.js-filter-closer', function () {
+  $('.games__filter').removeClass('is-open');
+  return false;
+});
+
+//тоглер модов на мобилах
+$(document).on('click', '.js-mods-toggler', function () {
+  if($('body').width() < 767) {
+    $(this).find('svg').toggleClass('rotate');
+    $(this).next('.mods__inner').slideToggle();
+  }
+  return false;
+});
+
+//тоггл брендов
+$(document).on('change', '.js-series', function () {
+  $('.mods__brands').slideToggle();
   return false;
 });
