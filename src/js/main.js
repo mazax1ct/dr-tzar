236622bbe8a82027086932ed295526723287a148
mazax1ct <vova@giveandget.ru>
2021-07-20 10:58:08 +0300

    reset + animate + no items + fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,81 @@
+const gamesSlider = new Swiper('.js-games-slider', {
+    loop: true,
+    navigation: {
+        nextEl: '.js-games-next',
+        prevEl: '.js-games-prev',
+    },
+    slidesPerView: 4,
+    spaceBetween: 4,
+    breakpoints: {
+        768: {
+            slidesPerView: 6,
+            spaceBetween: 0
+        },
+        1200: {
+            slidesPerView: 9,
+            spaceBetween: 4
+        },
+        1900: {
+            slidesPerView: 9,
+            spaceBetween: 8
+        },
+        2500: {
+            slidesPerView: 9,
+            spaceBetween: 16
+        }
+    }
+});
+
+const designSlider = new Swiper('.js-design-slider', {
+    loop: true,
+    navigation: {
+        nextEl: '.js-design-slider .swiper-button-next',
+        prevEl: '.js-design-slider .swiper-button-prev',
+    },
+    slidesPerView: 1,
+    observer: true,
+    observeParents: true
+});
+
+const configsSlider = new Swiper('.js-configs-slider', {
+    loop: false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+    navigation: {
+        nextEl: '.js-configs-next',
+        prevEl: '.js-configs-prev'
+    },
+    slidesPerView: 1,
+    breakpoints: {
+        1200: {
+            allowTouchMove: false,
+            shortSwipes: false,
+            simulateTouch: false,
+            slidesPerView: 2,
+            spaceBetween: 16
+        },
+        1900: {
+            allowTouchMove: false,
+            shortSwipes: false,
+            simulateTouch: false,
+            slidesPerView: 2,
+            spaceBetween: 56
+        },
+        2500: {
+            allowTouchMove: false,
+            shortSwipes: false,
+            simulateTouch: false,
+            slidesPerView: 2,
+            spaceBetween: 88
+        }
+    }
+});
+
 //тоглер параметров на мобилах
 $(document).on('click', '.js-params-toggler', function () {
   if($('body').width() < 767) {
@@ -64,14 +142,19 @@ $(document).on('click', '.js-card-settings-closer', function () {
 
 //переключение дизайн/цвет
 $(document).on('change', '.js-card-switch', function () {
+  var container = $(this).closest('.card');
   var parent = $(this).closest('.card__switch-block');
 
+
+  container.find('.card__slider.is-active').removeClass('animated visible');
   parent.find('.card__switch-radios.is-active').removeClass('animated'); //анимация движения
   setTimeout(function() {
     parent.find('.card__switch-radios.is-active').removeClass('visible'); //анимация фейда
     setTimeout(function() {
+      container.find('.card__slider').toggleClass('is-active');
       parent.find('.card__switch-radios').toggleClass('is-active'); //переключаем блок
       setTimeout(function() {
+        container.find('.card__slider.is-active').addClass('visible animated');
         parent.find('.card__switch-radios.is-active').addClass('visible'); //анимация фейда
         setTimeout(function() {
           parent.find('.card__switch-radios.is-active').addClass('animated'); //анимация движения
